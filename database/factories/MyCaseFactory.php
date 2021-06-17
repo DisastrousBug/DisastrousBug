732e00bb90bb9fa7477be963d2823743b9c80479
Artem <artem_bondar98@mail.ru>
2021-06-17 16:18:23 +0200

    git init

---
+++ database/factories/MyCaseFactory.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace Database\Factories;
+
+use App\Models\MyCase;
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+class MyCaseFactory extends Factory
+{
+    /**
+     * The name of the factory's corresponding model.
+     *
+     * @var string
+     */
+    protected $model = MyCase::class;
+
+    /**
+     * Define the model's default state.
+     *
+     * @return array
+     */
+    public function definition()
+    {
+        return [
+            //
+        ];
+    }
+}
---
+++ database/migrations/2021_06_17_133130_create_blogs_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateBlogsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('blogs', function (Blueprint $table) {
+            $table->id();
+            $table->timestamps();
+            $table->integer('user_id')->nullable(false);
+            $table->integer('metadata_id');
+            $table->text('body');
+            $table->string('title',255);
+            $table->string('slug',255);
+
+            $table->foreign(['user_id'])->references(['id'])->on('users')->cascadeOnUpdate()->cascadeOnDelete();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('blogs');
+    }
+}
